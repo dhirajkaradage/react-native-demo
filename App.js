001609cd0e7ad40416df53a492e49400e20dbab6
Dhiraj Karadage <dhiraj.karadage@amelioratesolutions.com>
2024-04-06 21:29:19 +0530

    Hello world project

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Button,
+  Modal,
+  StatusBar,
+  ActivityIndicator,
+} from "react-native";
+
+import { useState } from "react";
+
+const localImage = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [isVisible, setIsVisible] = useState(false);
+  function toggleStatusBar() {
+    setIsVisible(!isVisible);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "blue", padding: 50 }}>
+      <Button title="Press" onPress={() => toggleStatusBar()} color={"red"} />
+      {/* <Modal
+        visible={isVisible}
+        onRequestClose={() => setIsVisible(false)}
+        animationType="slide"
+        presentationStyle="pageSheet"
+      >
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 50 }}>
+          <Text>Modal Content</Text>
+          <Button title="Close" onPress={() => setIsVisible(false)} />
+        </View>
+      </Modal> */}
+      {/* <ScrollView>
+        <Text>
+          <Text style={{ color: "white" }}>Hello </Text>
+          World
+        </Text>
+
+        <Image source={localImage} style={{ height: 200, width: 300 }} />
+        <Text>
+          loremi adaptivesdf a sdf asd f adaptivesdfasd sdff adaptivesdfasddf
+          asdf a sdf as defaulta sd f\ adaptivesdf adaptivesdfasds defaultas
+          defaultasd fas defaultasdfasdf sadf adaptivesdfsadf adaptivesdfasddfa
+          sdfasd fasdf adaptivesdfasddfasdf sadffas defaultasdfasdfas fasasdf
+          adaptivesdfsadfdf asdf sadfasd fasd f asd fasdf asdf asdf a sdf sadf
+          asdf asfd asdf a sdfa df asdf asdf asd
+        </Text>
+        <Image source={localImage} style={{ height: 200, width: 300 }} />
+        <Image source={localImage} style={{ height: 200, width: 300 }} />
+        <Image
+          source={{ uri: "https://picsum.photos/300" }}
+          style={{ height: 300, width: 300 }}
+        />
+
+        <ImageBackground source={localImage}>
+          <Text style={{ fontSize: 48, textAlign: "center", color: "white" }}>
+            Hello World
+          </Text>
+        </ImageBackground>
+      </ScrollView> */}
+
+      {/* <StatusBar
+        backgroundColor={"red"}
+        barStyle={"light-content"}
+        hidden={isVisible}
+      /> */}
+
+      <ActivityIndicator />
+      <ActivityIndicator size={"large"} />
+      <ActivityIndicator size={"large"} color={"red"} />
+      <ActivityIndicator size={"large"} color={"red"} animating={isVisible} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
